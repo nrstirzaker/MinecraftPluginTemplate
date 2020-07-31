6061f2966e466bb7ebb5fe6b9bf76f9a576bc7f3
BuildTools <unconfigured@null.spigotmc.org>
2020-07-31 13:40:57 +0100

    added in proof of concept

--- src/main/java/com/foundrysoftware/minecraft/plugins/PluginTemplate.java
+++ src/main/java/com/foundrysoftware/minecraft/plugins/PluginTemplate.java
@@ -1,11 +1,14 @@
 package com.foundrysoftware.minecraft.plugins;
 
+import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.World;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.ArmorStand;
 import org.bukkit.entity.Player;
+import org.bukkit.event.entity.EntityShootBowEvent;
+import org.bukkit.event.entity.ProjectileHitEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -38,17 +41,15 @@ public class PluginTemplate extends JavaPlugin {
         }
         return false;
     }
-    :Override
-    public boolean onInteract(PlayerInteractEvent e){
-        if(e.getItem()!=null && e.getItem().getItemMeta().getDisplayName().contains("score")) {
-            Location loc = e.getClickedBlock().getLocation();
-            loc.setY(loc.getY() + 1);
-            World world = loc.getWorld();
-            ArmorStand a = world.spawn(loc, ArmorStand.class);
-            a.setVisible(false);
-            a.addScoreboardTag("score");
-            return true;
+    @Override
+    public boolean onShoot(ProjectileHitEvent e){
+        Location loc;
+        if(e.getHitBlock()!=null){
+            loc = e.getHitBlock().getLocation();
+        }else{
+            loc = e.getHitEntity().getLocation();
         }
-        return false;
+        Bukkit.getServer().broadcastMessage(loc.toString());
+        return true;
     }
 }
