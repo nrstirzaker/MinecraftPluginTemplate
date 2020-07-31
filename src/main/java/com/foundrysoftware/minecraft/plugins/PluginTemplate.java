1b7dd8dc4e49e4017758189a867a3ad8e262dbfa
BuildTools <unconfigured@null.spigotmc.org>
2020-07-31 17:44:16 +0100

    added /hawkeye

--- src/main/java/com/foundrysoftware/minecraft/plugins/PluginTemplate.java
+++ src/main/java/com/foundrysoftware/minecraft/plugins/PluginTemplate.java
@@ -2,6 +2,7 @@ package com.foundrysoftware.minecraft.plugins;
 
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
+import org.bukkit.Material;
 import org.bukkit.World;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
@@ -9,10 +10,27 @@ import org.bukkit.entity.*;
 import org.bukkit.event.entity.EntityShootBowEvent;
 import org.bukkit.event.entity.ProjectileHitEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.PlayerInventory;
+import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.inventory.meta.PotionMeta;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.potion.PotionData;
+import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
 
 import javax.validation.constraints.NotNull;
+
+import java.util.*;
+import java.util.List;
+import java.util.logging.Logger;
+import com.sun.org.apache.bcel.internal.generic.InstructionConstants;
+import org.bukkit.potion.PotionType;
+
+import java.util.HashMap;
+import java.util.*;
+import java.util.logging.Logger;
 
 public class PluginTemplate extends JavaPlugin {
 
@@ -33,12 +51,53 @@ public class PluginTemplate extends JavaPlugin {
     @Override
     public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, String[] args) {
         if (label.equalsIgnoreCase("simple")){
-            if (sender instanceof Player){
-                Player me = (Player)sender;
+            if (sender instanceof Player) {
+                Player me = (Player) sender;
                 me.sendMessage("Plugin Working");
                 return true;
             }
-        }
+                if (label.equalsIgnoreCase("hawkeye")){
+                    if(args[0].equalsIgnoreCase("tnt")){
+                        if(sender instanceof Player){
+                            Player me =(Player)sender;
+                            PlayerInventory inv = me.getInventory();
+                            for(int i = 0; i<inv.getSize(); i++){
+                                ItemStack item = inv.getItem(i);
+                                        if(item == null){
+                                            continue;
+                                        }
+                            if(item.getType()== Material.TIPPED_ARROW|| item.getType() == Material.ARROW || item.getType()==Material.SPECTRAL_ARROW){
+                                item.setType(Material.TIPPED_ARROW);
+                                item.setAmount(item.getAmount());
+                                PotionMeta im = (PotionMeta)item.getItemMeta();
+                                im.setBasePotionData(new PotionData(PotionType.JUMP));
+                                im.addCustomEffect(new PotionEffect(PotionEffectType.CONDUIT_POWER,10,10),true);
+                                item.setItemMeta(im);
+
+                            }
+                            }
+
+                        }
+                    }else{
+                        if(sender instanceof Player){
+                            Player me =(Player)sender;
+                            PlayerInventory inv = me.getInventory();
+                            for(int i = 0; i<inv.getSize(); i++){
+                                ItemStack item = inv.getItem(i);
+                                if(item == null){
+                                    continue;
+                                }
+                                if(item.getType()== Material.TIPPED_ARROW|| item.getType() == Material.ARROW || item.getType()==Material.SPECTRAL_ARROW){
+                                    item.setType(Material.TIPPED_ARROW);
+                                    item.setAmount(item.getAmount());
+                                    PotionMeta im = (PotionMeta)item.getItemMeta();
+                                    im.setBasePotionData(new PotionData(PotionType.getByEffect(PotionEffectType.getByName(args[0]))));
+                                    item.setItemMeta(im);
+                    }
+
+                }
+            }
+        }}}
         return false;
     }
     @Override
@@ -54,16 +113,17 @@ public class PluginTemplate extends JavaPlugin {
            }
            Bukkit.getServer().broadcastMessage(loc.toString());
            if (arrow.hasCustomEffect(PotionEffectType.CONDUIT_POWER)) {
-               loc.getWorld().createExplosion(loc,10);
+               loc.getWorld().createExplosion(loc,10,false,false,shooter);
            }
-           if(arrow.hasCustomEffect(PotionEffectType.DOLPHINS_GRACE)){
-               for(Entity ent: loc.getWorld().getNearbyEntities(loc,5,5,5)){
-                   if(ent instanceof LivingEntity){
-                       (Damageable)((LivingEntity) ent).damage(50, shooter);
-                   }
-               }
-           }
+           //if(arrow.hasCustomEffect(PotionEffectType.DOLPHINS_GRACE)){
+              // for(Entity ent: loc.getWorld().getNearbyEntities(loc,5,5,5)){
+                   //if(ent instanceof LivingEntity){
+                      // (Damageable)((LivingEntity) ent).damage(50, shooter);
+                   //}
+              // }
            return true;
-       }return false;
+           }
+           return false;
+       }
     }
 }
