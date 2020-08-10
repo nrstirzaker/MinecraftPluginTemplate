4f9c13b2c7a4b0d617de1b767db06fc2452ff78a
BuildTools <unconfigured@null.spigotmc.org>
2020-08-10 13:17:08 +0100

    made changes requested by @nrstirzaker

--- src/main/java/com/foundrysoftware/minecraft/plugins/PluginTemplate.java
+++ src/main/java/com/foundrysoftware/minecraft/plugins/PluginTemplate.java
@@ -142,7 +142,7 @@ public class PluginTemplate extends JavaPlugin {
         }
 
        }
-       public boolean areadamage(Location loc,int area,int damage,Entity shooter){
+       private boolean areadamage(Location loc,int area,int damage,Entity shooter){
         for(Entity entity: loc.getWorld().getNearbyEntities(loc,area,area,area)){
             if(entity instanceof LivingEntity){
                 ((LivingEntity) entity).damage(damage,shooter);
